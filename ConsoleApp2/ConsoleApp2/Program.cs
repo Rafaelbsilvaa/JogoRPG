d0f7b582e5eb96c657222560a96dbee9f468ec97
Rafaelbsilvaa <rafaelbezerra41@outlook.com>
2024-03-06 20:56:22 -0300

    one

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Bem-vindo ao Jogo!");
+Console.WriteLine("1. Iniciar Jogo");
+Console.WriteLine("2. Sair");
---
+++ cs.cs
@@ -0,0 +1,27 @@
+class program 
+{
+    static void Main(string[] args)
+    {
+        var casa = new Casa ();
+        casa.Moradores = 5;
+        casa.Quartos = "Grandes";
+        casa.concluido = false;
+        casa.altura = 8.17;
+        DadosDaCasa(casa);
+    }
+    public static void DadosDaCasa(Casa casa){
+        console.writeline(casa.altura);
+        console.writeline(casa.concluido);
+    }
+    public class construcao {
+        public string cor;
+        public double altura;
+        public double largura;
+        public bool concluido;
+}
+
+    public class casa : construcao{
+
+        public string Quartos;
+}
+}
